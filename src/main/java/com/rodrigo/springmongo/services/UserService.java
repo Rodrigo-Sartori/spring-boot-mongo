bc7c8d0e24ad2cbf0a38c1bbeee7cba88c3ea186
Rodrigo-Sartori <rodrigosartoriazevedo@gmail.com>
2019-11-05 17:21:24 -0300

    feita conexão com MongoDB via repository e service

---
+++ src/main/java/com/rodrigo/springmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.rodrigo.springmongo.services;
+
+
+import com.rodrigo.springmongo.domain.User;
+import com.rodrigo.springmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
